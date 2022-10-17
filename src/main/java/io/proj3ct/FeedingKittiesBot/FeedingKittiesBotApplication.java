86e9883105169552d29eb6b8184799371f086c27
Danila Shabanov <shabanov.danila.o@gmail.com>
2022-10-17 22:56:47 +0300

    initialize project

---
+++ src/main/java/io/proj3ct/FeedingKittiesBot/FeedingKittiesBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.FeedingKittiesBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedingKittiesBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedingKittiesBotApplication.class, args);
+    }
+
+}
