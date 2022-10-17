2465731d694bf06e1b8573b7dfa54dfe1e201ba3
Danila Shabanov <shabanov.danila.o@gmail.com>
2022-10-17 23:10:49 +0300

    feat: com.config.BotInitializer for start bot, and command /start

---
+++ src/main/java/com/config/BotInitializer.java
@@ -0,0 +1,30 @@
+package com.config;
+
+import com.service.TelegramBot;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInitializer {
+
+    TelegramBot bot;
+
+    public BotInitializer(TelegramBot bot) {
+        this.bot = bot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException exception) {
+            exception.getStackTrace();
+        }
+    }
+}
